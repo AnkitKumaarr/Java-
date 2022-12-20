f400227d970faca13fdbe9e55ecc30d9bc87d80a
Ankit Kumar <118204626+AnkitKumaarr@users.noreply.github.com>
2022-12-20 23:25:48 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,23 @@
+// class name is always in PascalCase
+public class function {
+    // void -- no return
+    // show -- it is the name of fn
+    // fname() --- banana bracket
+    // static -- when class is loaded static things will loaded
+
+    // call stack -----
+    // function goes in stack
+    // stack build 
+    // stack fall 
+    // Function name written in small letter with camelCase 
+    // return;  --  exit from the function
+    static void show(){ // start of function
+        // statements;
+        for(int i =1; i<10;i++ ){
+            System.out.println("Ankit"); // println is function 
+        }
+    }// end of funxtion
+    public static void main(String[] args) {
+        show();
+    }
+}
