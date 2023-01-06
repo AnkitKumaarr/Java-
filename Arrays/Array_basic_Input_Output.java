53abe18b7c3a6a221d6ecbca914c3c6af39576bb
Ankit Kumar <118204626+AnkitKumaarr@users.noreply.github.com>
2023-01-06 22:43:13 +0530

    Add files via upload

---
+++ Arrays/Array_basic_Input_Output.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Array_basic_Input_Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[4];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        //1st method -- for loop
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        // 2nd method -- enhanced loops
+        for (int num : arr) {
+            System.out.println(arr[num]);
+        }
+        //3rd method using toString() function
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+
+    }
+}
