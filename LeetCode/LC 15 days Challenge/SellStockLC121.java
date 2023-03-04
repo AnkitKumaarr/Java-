3c0c718205ef32889b70c9fd0b5fd5836d563fdf
Ankit Kumar <118204626+AnkitKumaarr@users.noreply.github.com>
2023-03-04 21:23:13 +0530

    Add files via upload

---
+++ LeetCode/LC 15 days Challenge/SellStockLC121.java
@@ -0,0 +1,12 @@
+public class SellStockLC121 {
+    public int maxProfit(int[] prices) {
+        int sellOne = 0;
+        int holdOne = Integer.MIN_VALUE;
+    
+        for (final int price : prices) {
+          sellOne = Math.max(sellOne, holdOne + price);
+          holdOne = Math.max(holdOne, -price);
+        }
+        return sellOne;
+        }
+}
