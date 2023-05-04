619d81ade397f2e2c9c3122d5e73b246ab40470b
Ankit Kumar <118204626+AnkitKumaarr@users.noreply.github.com>
2023-05-04 22:13:48 +0530

    Delete HybridInheritance.java

--- OOPs/HybridInheritance.java
+++
@@ -1,6 +0,0 @@
-public class HybridInheritance {
-    //  hybrid Inheritance yaani ki combination of Single Inheritance or Multiple Inheritance 
-    //  jaise Multiple Inheritance Nahi hota waise hi ... hybrid Inheritance Nahi hota 
-    //  pr hm kr sakte hai Interfaces ka use krke.............
-    
-}
