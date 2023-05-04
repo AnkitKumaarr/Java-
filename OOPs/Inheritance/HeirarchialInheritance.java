69bd6add6954b1b4fb1aad7e7857ba46bafb80af
Ankit Kumar <118204626+AnkitKumaarr@users.noreply.github.com>
2023-05-04 22:18:42 +0530

    Add files via upload

---
+++ OOPs/Inheritance/HeirarchialInheritance.java
@@ -0,0 +1,7 @@
+public class HeirarchialInheritance extends Box {
+    HeirarchialInheritance(){
+        System.out.println("I am a same base class .....   ");
+
+        //  heirarchial Inheritance matlab ki ek parent ke do alg alg child ek hi level pr....
+    }
+}
