b47fb3802ecdbe664f74142a61187f8f7c35f19a
Ankit Kumar <118204626+AnkitKumaarr@users.noreply.github.com>
2023-05-04 22:13:33 +0530

    Delete HeirarchialInheritance.java

--- OOPs/HeirarchialInheritance.java
+++
@@ -1,7 +0,0 @@
-public class HeirarchialInheritance extends Box {
-    HeirarchialInheritance(){
-        System.out.println("I am a same base class .....   ");
-
-        //  heirarchial Inheritance matlab ki ek parent ke do alg alg child ek hi level pr....
-    }
-}
